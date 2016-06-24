eaf715906d861558642c5ec7482e915c7211dca2
Adam <adamjbrowner@mgmail.com>
2016-06-24 09:22:55 +0100

    Added a more interactive menu

--- directory.rb
+++ directory.rb
@@ -5,11 +5,7 @@ def input_students
   name = gets.chomp
   while !name.empty? do
     students << {:name => name, :cohort => :november}
-    if students.count == 1
-      puts "Now we have 1 great student"
-    else
-      puts "Now we have #{students.count} great students"
-  end
+    puts "Now we have #{students.count} great student(s)"
     name = gets.chomp
   end
   students
@@ -27,10 +23,40 @@ def print(students)
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  if names.count == 1
+    puts "Overall, we have 1 great student"
+  else
+    puts "Overall, we have #{names.count} great students"
+  end
 end
 
-students = input_students
+def interactive_menu
+  students = []
+  loop do
+    #1. Print the menu and asks the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # 2. Read the input and save it into a variable
+    selection = gets.chomp
+    # 3. Do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what that means, try again"
+    end
+  end
+end
+
+
+interactive_menu
 #Call the methods
 print_header
 print(students)
