c70bbf2e51744b04f844ae1c686f7442df345f96
Adam <adamjbrowner@mgmail.com>
2016-06-24 09:35:29 +0100

    Refactored more of the code

--- directory.rb
+++ directory.rb
@@ -1,14 +1,14 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.chomp
   while !name.empty? do
-    students << {:name => name, :cohort => :november}
-    puts "Now we have #{students.count} great student(s)"
+    @students << {:name => name, :cohort => :november}
+    puts "Now we have #{@students.count} great student(s)"
     name = gets.chomp
   end
-  students
 end
 
 def print_header
@@ -16,8 +16,8 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |students|
+def print_students_list(students)
+  @students.each do |students|
     puts "#{students[:name]} (#{students[:cohort].capitalize} cohort)"
   end
 end
@@ -30,28 +30,35 @@ def print_footer(names)
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list(@students)
+  print_footer(@students)
+end
+
+def process(selection)
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what that means, try again"
+  end
+end
+
 def interactive_menu
-  students = []
   loop do
-    #1. Print the menu and asks the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. Read the input and save it into a variable
-    selection = gets.chomp
-    # 3. Do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what that means, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
